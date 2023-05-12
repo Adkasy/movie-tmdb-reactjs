d3fb2baabd21218ed28f365db29582d4e2146089
Aditya <adityakasyidi09@gmail.com>
2023-05-13 02:15:53 +0700

    add NowPlaying component

--- movie-tmdb/src/pages/NowPlaying.js
+++ movie-tmdb/src/pages/NowPlaying.js
@@ -1,18 +1,49 @@
 import React from "react"
 import { useState, useEffect } from "react"
+import axios from "axios"
 
 const NowPlaying = () => {
 	const [nowPlaying, setNowPlaying] = useState([])
+
 	useEffect(() => {
 		fetch(
 			"https://api.themoviedb.org/3/movie/now_playing?api_key=b219cb5c365b97f33e29ec55c592ee3e&language=en-US&page=1"
 		)
-			.then((response) => response.json())
+			.then((response) => {
+				if (response.ok) {
+					return response.json()
+				}
+				throw new Error("Network response was not ok.")
+
+				//! Ini cara lain menggunakan ternary opeartion (alternatif aja)
+				// return response.ok ? response.json() : Error("Error nih")
+			})
+
 			.then((data) => {
-				console.log(data.results)
+				// console.log(data, "<-- Ini data masih dalam bentuk mentah")
+				// console.log(data.results, "<--- Ini hasil jadinya")
 				setNowPlaying(data.results)
 			})
+
+			.catch((error) => {
+				console.error("There was a problem with the fetch operation:", error)
+			})
 	}, [])
+
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const response = await axios.get(
+	// 				"https://api.themoviedb.org/3/movie/now_playing?api_key=b219cb5c365b97f33e29ec55c592ee3e&language=en-US&page=1"
+	// 			)
+	// 			console.log(response.data.results, "Ini fetching dengan axios")
+	// 			setNowPlaying(response.data.results)
+	// 		} catch (error) {
+	// 			console.log(error)
+	// 		}
+	// 	}
+	// 	fetchData()
+	// }, [])
 
 	return (
 		<>
